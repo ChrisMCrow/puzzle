f88e0db7cab0a00abd42bb0cba6c5ad8656e1d1c
Chris Crow and Derek Smith <derek@>
2018-08-14 16:33:34 -0700

    fixes css bug. Adds show/hide functionality for the results and the form.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var vowels=["a","e","i","o","u"];
+
+$(document).ready(function(){
+  $("form button").click(function(event) {
+    event.preventDefault();
+
+    var userInput = $("#userInput").val();
+    var userInputs = userInput.split("");
+
+    for (i = 0; i < userInput.length; i++) {
+      for (v = 0; v < vowels.length; v++) {
+        if (userInputs[i] === vowels[v]) {
+          userInputs[i] = "-";
+        }
+      }
+    }
+
+    var output = userInputs.join("");
+    $("form").hide();
+    $("#output").append("<h1>" + output + "</h1>");
+  });
+});
